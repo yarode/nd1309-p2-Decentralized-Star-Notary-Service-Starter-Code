4c4f7814c37ec89d594e68e8850ee97606b3b6b1
yarode <jaspervdvorst@gmail.com>
2021-07-26 04:37:03 +0200

    oops

--- truffle-config.js
+++ truffle-config.js
@@ -46,20 +46,18 @@ module.exports = {
     //
 
     //Rinkeby settings
-    networks: {
-      development: {
-        host: "127.0.0.1",
-        port: 8545,
-        network_id: "*",
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA_URL);
       },
-      rinkeby: {
-        provider: function() {
-          return new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA_URL);
-        },
-        network_id: 4,
-        gas: 4500000,
-        gasPrice: 10000000000,
-      }
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
     }
 
     // Another network with more advanced options...
